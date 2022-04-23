c98c641a4f6aa258ac95b826f5955b3adeb948bb
alb-soul <100475822+alb-soul@users.noreply.github.com>
2022-04-23 22:04:57 +0700

    Tugas Kelompok PWeb

---
+++ Tugas Kelompok PWeb/target.php
@@ -0,0 +1,11 @@
+<?php
+	echo "Helllo";
+	echo "Username : ".$_POST['username']."<br />";
+	echo "Password : ".$_POST['password']."<br />";
+	echo "kode : ".$_POST['kode']."<br />";
+	echo "name : ".$_POST['name']."<br />";
+	echo "nohp : ".$_POST['nohp']."<br />";
+	echo "jeniskelamin : ".$_POST['jeniskelamin']."<br />";
+	echo "provinsi : ".$_POST['provinsi']."<br />";
+	echo "alamat : ".$_POST['alamat']."<br />";
+?>
